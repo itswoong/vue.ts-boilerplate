1b1545312cdf81a6c7cc7a2580019c55fbbefec4
HaruHNi <haruhni@lottecard.co.kr>
2021-01-21 18:53:43 +0900

    husky

--- prepush.sh
+++
@@ -1,2 +0,0 @@
-echo "hi!!"
-exit 1
---
+++ vetur.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    setting: {
+        "vetur.useWorkspaceDependencies": true,
+        "vetur.experimental.templateInterpolationService": true
+    }
+}
